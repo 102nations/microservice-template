aa19be50e66ca3a803347cdde727775be766be5a
David Sam <david.sam@gmail.com>
2024-05-23 09:21:10 -0500

    added template

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # microservice-template
+
+az ad sp create-for-rbac \
+    --role contributor \
+    --scopes /subscriptions/f2ecdfbd-c9a8-424d-9fd6-b9662a147ca8 \
+    --json-auth
+    
+az ad sp create-for-rbac \
+    --role contributor \
+    --scopes /subscriptions/f2ecdfbd-c9a8-424d-9fd6-b9662a147ca8/resourceGroups/rg-project-sb1 \
+    --json-auth    
---
+++ src/test/java/com/one02nations/template/controller/TemplateControllerTest.java
@@ -0,0 +1,65 @@
+package com.one02nations.template.controller;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import jakarta.annotation.PostConstruct;
+
+/**
+ * Integration tests to verify that our endpoints are properly secured.
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+class TemplateControllerTest {
+
+	static MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext controller;
+
+
+	@PostConstruct
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(controller).apply(springSecurity()).build();
+	}
+
+	@MockBean
+	@SuppressWarnings("unused")
+	private JwtDecoder jwtDecoder;
+
+	@Test
+	public void testPrivateEndpointReturnsUnauthorizedWhenCalled() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(get("/api/v1/me")).andDo(print())
+				.andExpect(status().isUnauthorized()).andReturn();
+	}
+
+	@Test
+	@WithMockUser(username = "testUser")
+	public void testPrivateEndpointReturnsOkWhenAuthorized() throws Exception {
+
+		MvcResult mvcResult = mockMvc.perform(get("/api/v1/test")).andDo(print())
+				.andExpect(status().isOk()).andReturn();
+		assertNotNull(mvcResult.getResponse().getContentAsString());
+
+	}
+}
