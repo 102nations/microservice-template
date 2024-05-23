59e817de5bcfdcb529d2012629a6175fc4f0f859
David Sam <david.sam@gmail.com>
2024-05-23 11:35:53 -0500

    updated security profiles

--- src/test/java/com/one02nations/template/controller/TemplateControllerTest.java
+++ src/test/java/com/one02nations/template/controller/TemplateControllerTest.java
@@ -1,7 +1,6 @@
 package com.one02nations.template.controller;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -14,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,15 +21,15 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
 import jakarta.annotation.PostConstruct;
 
 /**
  * Integration tests to verify that our endpoints are properly secured.
  */
-@SpringBootTest
+@SpringBootTest()
 @RunWith(SpringRunner.class)
 @ContextConfiguration
+@ActiveProfiles("test")
 class TemplateControllerTest {
 
 	static MockMvc mockMvc;
